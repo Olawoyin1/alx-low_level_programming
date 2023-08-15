fcc01d0f2c3c7ead307930f6d5f37c941a714e0f
Yusuf Olawoyin <yustee2017@gmail.com>
2023-08-15 14:32:59 -0700

    task 3 : write alphabet 10 times with function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this function will print the alphabet 10 tmes \
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
